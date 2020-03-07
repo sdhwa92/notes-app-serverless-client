9c81dc1baecbb5211529511bc9c50c600255afdd
Daehwa Seo <sdhwa92@gmail.com>
2020-03-07 17:43:10 +1000

    AWS Amplify configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "daehwa-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://yet74np3rc.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_mWJjaPj2O",
+    APP_CLIENT_ID: "1he21n7fqbn4g47bufnsogknnd",
+    IDENTITY_POOL_ID: "ap-southeast-2:0b0465d5-5cb3-4bc3-8fee-0f6ef69e3c79"
+  }
+}
