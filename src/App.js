ec11242bb117af6c3a740b5e7f6fc4a076f83a04
Daehwa Seo <sdhwa92@gmail.com>
2020-03-07 17:19:37 +1000

    Add not found page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
 import Routes from "./Routes";
 import './App.css';
 
@@ -14,6 +16,16 @@ function App(props) {
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to={"/signup"}>
+              <NavItem>Sign up</NavItem>
+            </LinkContainer>
+            <LinkContainer to={"/login"}>
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
