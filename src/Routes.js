682489b8c4e91148fd34c6f2949c2f36657883ee
Daehwa Seo <sdhwa92@gmail.com>
2020-03-11 19:54:20 +1000

    Add session to the state

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path={"/"} exact component={Home} />
-      <Route path={"/login"} exact component={Login} />
+      <AppliedRoute path={"/"} exact component={Home} appProps={appProps} />
+      <AppliedRoute path={"/login"} exact component={Login} appProps={appProps} />
 
       /* Finally, catch all unmatched routes */
-      <Route component={NotFound} />
+      <AppliedRoute component={NotFound} />
     </Switch>
   );
 }
