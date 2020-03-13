b1a6f9b06d3d2a73099597e3f5c8b36bfcd63c83
Daehwa Seo <sdhwa92@gmail.com>
2020-03-14 09:01:59 +1000

    Create cusome react hook
    - created a custom react hook to handle form fields

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { Auth } from "aws-amplify";
 import { Link } from "react-router-dom";
+import { useFormFields } from "../libs/hooksLib";
 
 /*Components*/
 import LoaderButton from "../components/LoaderButton";
@@ -10,12 +11,14 @@ import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 
 export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
@@ -24,7 +27,7 @@ export default function Login(props) {
     setIsLoading(true);
 
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       props.userHasAuthenticated(true);
       props.history.push("/");
     } catch (e) {
@@ -41,16 +44,16 @@ export default function Login(props) {
           <FormControl
             autoFocus
             type={"email"}
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId={"password"} bsSize={"large"}>
           <ControlLabel>Password</ControlLabel>
           <FormControl
             type={"password"}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <Link to="/login/reset">Forgot password?</Link>
