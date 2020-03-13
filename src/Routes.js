9e8c6f150064397c0301950bb5898b1199f2a1d3
Daehwa Seo <sdhwa92@gmail.com>
2020-03-14 08:47:17 +1000

    Add change email page

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,7 @@ import NotFound from "./containers/NotFound";
 import ResetPassword from "./containers/ResetPassword";
 import Settings from "./containers/Settings";
 import ChangePassword from "./containers/ChangePassword";
+import ChangeEmail from "./containers/ChangeEmail";
 
 export default function Routes({ appProps }) {
   return (
@@ -20,6 +21,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path={"/login/reset"} exact component={ResetPassword} appProps={appProps} />
       <AppliedRoute path={"/settings"} exact component={Settings} appProps={appProps}/>
       <AppliedRoute path={"/settings/password"} exact component={ChangePassword} appProps={appProps} />
+      <AppliedRoute path={"/settings/email"} exact component={ChangeEmail} appProps={appProps} />
 
       /* Finally, catch all unmatched routes */
       <AppliedRoute component={NotFound} />
